da8a3820844b86ba7e3d5a9373864b9fc3a07438
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-05-16 16:29:22 +0300

    6.1

---
+++ src/Exercises/6.1.jsx
@@ -0,0 +1,27 @@
+import Card from "./4.2 comp/card";
+
+function Exercises() {
+  return (
+    <div>
+      <Card
+        img="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+        title="nice place"
+        text="come vist us"
+      />
+      <br />
+      <Card
+        img="https://media-cdn.tripadvisor.com/media/photo-s/0a/e4/0e/e2/nice-views-and-love-heart.jpg"
+        title="nice place to visit"
+        text="visit our website"
+      />
+      <br />
+      <Card
+        img="https://cdn.cheapism.com/images/011618_most_beautiful_views_in_the_world_sli.max-784x410_euInqPB.jpg"
+        title="nice view"
+        text="come see the view"
+      />
+    </div>
+  );
+}
+
+export default Exercises;
