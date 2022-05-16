e02f9d0f1603a977cd2c3a71866686a5f2a25fc7
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-05-16 16:29:27 +0300

    6.1

--- src/Exercises/4.2 comp/card.jsx
+++ src/Exercises/4.2 comp/card.jsx
@@ -1,15 +1,30 @@
+import React from "react";
 import "./4.2.css";
-function Card(obj) {
-  return (
-    <div className="card">
-      <img src={obj.img} alt="img" />
-      <p>{obj.title}</p>
-      <p>{obj.text}</p>
-      <div>
-        <button>share</button> <button>explore</button>
+// function Card(obj) {
+//   return (
+//     <div className="card">
+//       <img src={obj.img} alt="img" />
+//       <p>{obj.title}</p>
+//       <p>{obj.text}</p>
+//       <div>
+//         <button>share</button> <button>explore</button>
+//       </div>
+//     </div>
+//   );
+// }
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <img src={this.props.img} alt="img" />
+        <p>{this.props.title}</p>
+        <p>{this.props.text}</p>
+        <div>
+          <button>share</button> <button>explore</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
